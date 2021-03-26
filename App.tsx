0181bbad18b4e813972143593d4a194918f7a605
chescotech <choolwe@chesco-tech.com>
2021-03-26 04:54:22 +0200

    ini

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import AppFrame from "./Frame";
 import { Provider } from "react-redux";
 import store from "./AppStore";
-
+import FlashMessage from "react-native-flash-message";
 
 export default function App() {
   return (
     <Provider store={store}>
       <AppFrame />
+      <FlashMessage duration={2850} position="top" />
     </Provider>
   );
 }
